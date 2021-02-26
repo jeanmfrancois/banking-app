705d609cf9371df623fa412ccec144b30d7a950e
Jean francois Nepton <jeanmfrancois@hotmail.com>
2021-02-25 19:51:58 -0500

    Create Spring Initialization

---
+++ src/test/java/com/jfbuilds/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfbuilds.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
